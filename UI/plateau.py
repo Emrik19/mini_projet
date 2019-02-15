c8b39abdb40f5aa78fa92c7cafcd9078b6598d83
Francois Bechet <francois.bechet@users.noreply.github.com>
2019-02-15 10:16:26 +0100

    Update UI/plateau.py

---
+++ UI/plateau.py
@@ -0,0 +1,8 @@
+print(("╔"),end=""),print(("═"*28),end=""),print("╗",)
+n=0
+while n<28:
+    print("║",(("■")*18),"║")
+    n=n+1
+print(("╚"),end=""),print("═"*28,end=""),print("╝")
+print("░") #éperon rocheux
+print("♀")  #héros
